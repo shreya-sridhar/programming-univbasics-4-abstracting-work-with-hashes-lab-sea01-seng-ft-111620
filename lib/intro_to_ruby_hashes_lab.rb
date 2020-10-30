c7beeb6190ea7b31acacf972a016e00b5a26a351
Shreya Sridhar <shreyamohak@outlook.com>
2020-10-30 21:53:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] = hash[key] + 1
-  else
-    hash[key] = 1
-  end
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
 end
